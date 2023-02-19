f41e5fab3071e65a3c76db7b0bc0da1f12002c3a
David R. Myers <davidrmyersii@gmail.com>
2023-02-19 16:15:40 -0500

    Export a LanguageDescription for the mindmap language

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { LRLanguage, LanguageSupport } from '@codemirror/language';
+import { LRLanguage, LanguageDescription, LanguageSupport } from '@codemirror/language';
 import { parser as mindmapParser } from './mindmap/mindmap.grammar';
 export { mindmapTags } from './mindmap/tags';
 
 export const mindmapLanguage = LRLanguage.define({
   name: 'mindmap',
   parser: mindmapParser,
+});
+
+export const mindmapLanguageDescription = LanguageDescription.of({
+  name: 'mermaid',
+  load: async () => {
+    return mindmap();
+  },
 });
 
 export function mindmap() {
