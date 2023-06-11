6e0816f4738efe3c3979ca3044e78480cc998d71
Alex Titarenko <alx.titarenko@gmail.com>
2023-06-10 20:05:37 -0700

    make sure standard tags have parent

--- src/tags/index.ts
+++ src/tags/index.ts
@@ -1,7 +1,11 @@
-import { Tag } from '@lezer/highlight';
+import { Tag, tags as t } from '@lezer/highlight';
+
+export const mermaidTags = {
+  diagramName: Tag.define(t.typeName)
+}
 
 export const mindmapTags = {
-  diagramName: Tag.define(),
+  diagramName: Tag.define(mermaidTags.diagramName),
   lineText1: Tag.define(),
   lineText2: Tag.define(),
   lineText3: Tag.define(),
@@ -10,11 +14,11 @@ export const mindmapTags = {
 };
 
 export const pieTags = {
-  diagramName: Tag.define(),
-  lineComment: Tag.define(),
-  number: Tag.define(),
-  showData: Tag.define(),
-  string: Tag.define(),
-  title: Tag.define(),
-  titleText: Tag.define(),
+  diagramName: Tag.define(mermaidTags.diagramName),
+  lineComment: Tag.define(t.lineComment),
+  number: Tag.define(t.number),
+  showData: Tag.define(t.keyword),
+  string: Tag.define(t.string),
+  title: Tag.define(t.keyword),
+  titleText: Tag.define(t.string),
 };
