6c8837112aacd88079f6b2b2f8e5ea8a28638249
inspirnathan <inspirnathan@gmail.com>
2023-09-07 19:32:52 -0500

    Prevent code folds that start on empty lines

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -1,6 +1,6 @@
 import { foldService } from '@codemirror/language';
 
-function countLeadingSpaces(str: string) {
+const countLeadingSpaces = (str: string) => {
   let count = 0;
 
   for (let i = 0; i < str.length; i++) {
@@ -14,21 +14,30 @@ function countLeadingSpaces(str: string) {
   }
 
   return count;
-}
+};
+
+const isEmptyLine = (text: string) => {
+  return /^[ \t]*$/.test(text);
+};
 
 export const foldByIndent = () => {
   return foldService.of((state, lineStart, lineEnd) => {
     const line = state.doc.lineAt(lineStart);
     const lineCount = state.doc.lines;
+
     let indents = countLeadingSpaces(line.text);
     let foldStart = lineStart;
     let foldEnd = lineEnd;
-
     let nextLine = line;
+
     while (nextLine.number < lineCount) {
       nextLine = state.doc.line(nextLine.number + 1);
+
+      if (nextLine.text === '' || isEmptyLine(nextLine.text)) continue;
+
       let nextIndents = countLeadingSpaces(nextLine.text);
-      if (nextIndents > indents) {
+
+      if (nextIndents > indents && !isEmptyLine(nextLine.text)) {
         foldEnd = nextLine.to;
       } else {
         break;
@@ -42,6 +51,11 @@ export const foldByIndent = () => {
     }
 
     foldStart = line.to;
+    const lineAtFoldStart = state.doc.lineAt(foldStart);
+
+    if (lineAtFoldStart.text === '' || isEmptyLine(lineAtFoldStart.text)) {
+      return null;
+    }
 
     return { from: foldStart, to: foldEnd };
   });
